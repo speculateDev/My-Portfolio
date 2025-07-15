99e834757d3103e80c35819e1b33b0a20bbbb58b
Speculate Dev <sida3li777@gmail.com>
2025-07-15 17:54:04 +0100

    Init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Front end developer portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} antialiased font-inter relative`}>
+        {children}
+      </body>
+    </html>
+  );
+}
